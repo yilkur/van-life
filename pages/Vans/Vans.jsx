84cadc9fb145a1f006054e96f612355c6a5b19e1
Yilmaz Kurnazcan <ykurnazcan@tocco.ch>
2025-05-20 19:08:24 +0200

    add error handling

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,16 +1,29 @@
 import React from "react"
 import { Link, useSearchParams } from "react-router-dom"
+import { getVans } from "../../api"
 
 export default function Vans() {
     const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+    const [loading, setLoading] = React.useState(false)
+    const [error, setError] = React.useState(null)
 
     const typeFilter = searchParams.get("type")
 
     React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))
+        async function loadVans() {
+            setLoading(true)
+            try {
+                const data = await getVans()
+                setVans(data)
+            } catch (err) {
+                setError(err)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        loadVans()
     }, [])
 
     const displayedVans = typeFilter
@@ -19,11 +32,11 @@ export default function Vans() {
 
     const vanElements = displayedVans.map(van => (
         <div key={van.id} className="van-tile">
-            <Link 
-                to={van.id} 
-                state={{ 
-                    search: `?${searchParams.toString()}`, 
-                    type: typeFilter 
+            <Link
+                to={van.id}
+                state={{
+                    search: `?${searchParams.toString()}`,
+                    type: typeFilter
                 }}
             >
                 <img src={van.imageUrl} />
@@ -45,6 +58,14 @@ export default function Vans() {
             }
             return prevParams
         })
+    }
+
+    if (loading) {
+        return <h1>Loading...</h1>
+    }
+    
+    if (error) {
+        return <h1>There was an error: {error.message}</h1>
     }
 
     return (
